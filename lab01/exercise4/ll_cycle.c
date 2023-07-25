67f5fb218b270f3337c0dcc6e7a9c82b60b1bff0
user103 <mabdullah.bee19seecs@seecs.edu.pk>
2023-07-25 12:12:37 +0500

    TASK01 Ex 3 and 4 Completed

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,22 @@
 #include <stddef.h>
-#include "ll_cycle.h"
+#include "/home/user103/su21-lab-starter/lab01/exercise4/ll_cycle.h"
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+  if ( head == NULL ) {
+    return 0;
+  }
+
+  node *fast_ptr, *slow_ptr;
+  slow_ptr = head;
+  fast_ptr = head->next;
+
+  while ( fast_ptr != NULL && fast_ptr->next != NULL ) {
+    if (fast_ptr == slow_ptr)
+      return 1;
+
+    slow_ptr = slow_ptr->next;
+    fast_ptr = fast_ptr->next->next;
+  }
+  return 0;
 }
